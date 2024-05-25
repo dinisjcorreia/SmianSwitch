1e2883a2ca49ace2723dc83fb3d33b01a3a68a31
Dinis Correia <151139018+dinisjcorreia@users.noreply.github.com>
2024-05-25 17:38:39 +0100

    menu

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -129,7 +129,7 @@ public class PlayerController : MonoBehaviour
             }
 
             if (SceneManager.GetActiveScene().name == "Main" ){
-                 if (gameObject.transform.position.y < 3){
+                 if (gameObject.transform.position.y < 1){
                 RestartScene();
             }
 
